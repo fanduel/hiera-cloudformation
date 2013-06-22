fae60c6dd9b7e8602a3bd0b6cc5b724574d0485c
Hugh Cole-Baker <sigmaris@gmail.com>
2013-06-22 12:22:50 +0100

    Fix copyright notice

--- lib/hiera/backend/cloudformation_backend.rb
+++ lib/hiera/backend/cloudformation_backend.rb
@@ -1,5 +1,5 @@
 =begin
-Copyright [yyyy] [name of copyright owner]
+Copyright 2013 FanDuel Ltd.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
